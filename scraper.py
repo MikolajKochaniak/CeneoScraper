e42fcb79b0564f3ecda549b4641874e5727c75b8
MikolajKochaniak <kochaniakmikolaj@gmail.com>
2023-04-17 16:33:16 +0200

    2023-17-04 poprawka plik json

--- scraper.py
+++ scraper.py
@@ -1,10 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+
+selectors = {
+    "opinion_id":[None,"data-entry-id"],
+    "author":["span.user-post__author-name"],
+    "recommendation":["span.user-post__author-recomendation > em"],
+    "stars":["span.user-post__score-count"],
+    "purchased":["div.review-pz"],
+    "opinion_date":["span.user-post__published > time:nth-child(1)","datetime"],
+    "purchase_date":["span.user-post__published > time:nth-child(2)","datetime"],
+    "useful":["button.vote-yes","data-total-vote"],
+    "unuseful":["button.vote-no","data-total-vote"],
+    "content":["div.user-post__text"],
+    "cons": ["div.review-feature__title--negatives ~ div.review-feature__item", None, True],
+    "pros": ["div.review-feature__title--negatives ~ div.review-feature__item", None, True],
+}
+
+
+
 def get_element(ancestor,selector=None, attribute = None, return_list=False):
     try:
         if return_list:
-            return [tag.text.strip() for tag in ancestor.select(selector)]
+            return [tag.text.strip() for tag in ancestor.select(selector)].copy()
         if not selector and attribute:
             return ancestor[attribute]
         if attribute:
@@ -12,34 +30,23 @@ def get_element(ancestor,selector=None, attribute = None, return_list=False):
         return ancestor.select_one(selector).text.strip()
     except (ArithmeticError,TypeError):
         return None
+
+
 #product_code = input("Podaj kod produktu: ")
-product_code = "22409122"
+product_code = "123849599"
 url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
 response = requests.get(url)
-page_dom = BeautifulSoup(response.text,'html.parser')
+page_dom = BeautifulSoup(response.text,"html.parser")
 opinions = page_dom.select("div.js_product-review")
 all_opinions = []
 for opinion in opinions:
-    single_opinion = {
-        "opinion_id":[None,"data-entry-id"],
-        "author":["span.user-post__author-name"],
-        "recommendation":["span.user-post__author-recomendation > em"],
-        "stars":["span.user-post__score-count"],
-        "purchased":["div.review-pz"],
-        "opinion_date":["span.user-post__published > time:nth-child(1)","datetime"],
-        "purchase_date":["span.user-post__published > time:nth-child(2)","datetime"],
-        "useful":["button.vote-yes","data-total-vote"],
-        "unuseful":["button.vote-no","data-total-vote"],
-        "content":["div.user-post__text"],
-        "cons": ["div.review-feature__title--negatives ~ div.review-feature__item",None,True],
-        "pros": ["div.review-feature__title--negatives ~ div.review-feature__item",None,True],
-
-
-    }
+    single_opinion = {}
+    for key, value in selectors.items():
+        single_opinion[key] = get_element(opinion, *value)
     all_opinions.append(single_opinion)
 
-with open("./opinion/{product_code}.json", "w", encoding = "UTF-8") as jf:
-    print(json.dumps(all_opinions,indent=4, ensure_ascii=False))
+with open(f"./opinions/{product_code}.json", "w", encoding="UTF-8") as jf:
+    json.dumps(all_opinions,indent=4, ensure_ascii=False)
     
 
 
